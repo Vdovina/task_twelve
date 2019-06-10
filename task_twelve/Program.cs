cac0a2039edecd53f0e41aeb93a3311b8fd667d4
stayl <staylimmk@gmail.com>
2019-06-10 18:33:30 +0500

    Добавьте файлы проекта.

---
+++ task_twelve/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_twelve
+{
+    class Program
+    {
+        static int count1 = 0, count2 = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сортировка слиянием");
+            Console.Write("Введите элементы массива: ");
+            var s = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            int[] array = new int[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                array[i] = Convert.ToInt32(s[i]);
+            }
+
+            Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", MergeSort(array)));
+            Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", BucketSort(array)));
+            Console.WriteLine($"Количество перестановок: для метода слияния - {count1}, для блочного метода - {count2}.");
+
+            Console.ReadLine();
+        }
+
+
+        //метод для слияния массивов
+        static void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            int left = lowIndex;
+            int right = middleIndex + 1;
+            int[] tempArray = new int[highIndex - lowIndex + 1];
+            int index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (array[left] < array[right])
+                {
+                    tempArray[index] = array[left];
+                    left++;
+                    count1++;
+                }
+                else
+                {
+                    tempArray[index] = array[right];
+                    right++;
+                    count1++;
+                }
+
+                index++;
+            }
+
+            for (int i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+                count1++;
+            }
+
+            for (int i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+                count1++;
+            }
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                array[lowIndex + i] = tempArray[i];
+                count1++;
+            }
+        }
+
+        //сортировка слиянием
+        static int[] MergeSort(int[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+
+            return array;
+        }
+
+        static int[] MergeSort(int[] array)
+        {
+            return MergeSort(array, 0, array.Length - 1);
+        }
+
+        static int[] BucketSort(int[] items)
+        {
+            // Предварительная проверка элементов исходного массива
+            if (items == null || items.Length < 2)
+                return items;
+
+            // Поиск элементов с максимальным и минимальным значениями
+            int maxValue = items[0];
+            int minValue = items[0];
+
+            for (int i = 1; i < items.Length; i++)
+            {
+                if (items[i] > maxValue)
+                    maxValue = items[i];
+
+                if (items[i] < minValue)
+                    minValue = items[i];
+            }
+
+            // Создание временного массива "карманов" в количестве,
+            // достаточном для хранения всех возможных элементов,
+            // чьи значения лежат в диапазоне между minValue и maxValue.
+            // Т.е. для каждого элемента массива выделяется "карман" List<int>.
+            // При заполнении данных "карманов" элементы исходного не отсортированного массива
+            // будут размещаться в порядке возрастания собственных значений "слева направо".
+            //
+
+            List<int>[] bucket = new List<int>[maxValue - minValue + 1];
+
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<int>();
+            }
+
+            // Занесение значений в пакеты
+            for (int i = 0; i < items.Length; i++)
+            {
+                bucket[items[i] - minValue].Add(items[i]);
+            }
+
+            // Восстановление элементов в исходный массив из карманов в порядке возрастания значений
+            int position = 0;
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                if (bucket[i].Count > 0)
+                {
+                    for (int j = 0; j < bucket[i].Count; j++)
+                    {
+                        items[position] = bucket[i][j];
+                        position++;
+                        count2++;
+                    }
+                }
+            }
+            return items;
+        }
+    }
+}
